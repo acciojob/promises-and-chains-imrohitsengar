fa55f6665687f4c12826bcb4186acf7bf38088c9
Rohit Singh Sengar <rsengar257@gmail.com>
2026-09-10 22:34:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+let form=document.getElementById('form');
+let btn=document.getElementById('btn');
+form.addEventListener('submit',(e)=>{
+	e.preventDefault();
+	let age=document.getElementById('age');
+	let name=document.getElementById('name');
+	if( age.value==="" || name.value===""){
+		alert("Please enter valid details.")
+		return;
+	}
+	const promise= new Promise((res,rej)=>{
+		if(age.value>18){
+			setTimeout(()=>{
+				res("Welcome, . You can vote.")
+			},4000)
+		}else{
+			setTimeout(()=>{
+				rej("Oh sorry . You aren't old enough.")
+			},4000)
+		}
+	})
+	promise.then((res)=> alert(`${res}`))
+		.catch((rej)=> alert(`${rej}`))
+})
